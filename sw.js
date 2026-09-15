77a90bf1d3e208753d962d3250689aad48ecfeed
husinyaher-ctrl <husinyaher@gmail.com>
2026-09-15 18:06:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,6 @@
-const CACHE_NAME = 'arenagamers-v2';
-const ASSETS = [
-  './index.html',
-  './play.html',
-  './privacy.html',
-  './terms.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './assets/js/games-feed.js'
-];
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  // Jangan cache request ke feed GameMonetize - selalu ambil data terbaru dari jaringan.
-  if (e.request.url.includes('gamemonetize')) {
-    e.respondWith(fetch(e.request));
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11807588
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
