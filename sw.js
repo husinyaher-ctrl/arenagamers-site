06c982085f4e13948af095733b14d03429ea49d1
husinyaher-ctrl <husinyaher@gmail.com>
2026-09-16 16:40:25 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-// ArenaGamers - clean dummy SW for GameMonetize review
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  self.registration.unregister();
-  console.log('SW cleaned');
-});
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
